2d6c819ad9d07598bd46633fe61a74f54a93bdd0
BrandomNV <noriegaverab@gmail.com>
2022-06-27 16:25:21 -0500

    RUTA: / - homepage

---
+++ lang/en/homepage.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    // Route : /
+    'simple_url'   => 'Simple URL shortener',
+    'for_individuals' => 'for individuals',
+    'bussinesses' => 'businesses',
+    'search_link' => 'Shorten your link',
+    'shorten' => 'Shorten',
+    'custom_url' => 'Custom URL (optional)',
+    'replace_url' => 'Replace clunky URLs with meaningful short links that get more clicks.',
+];
--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -18,8 +18,8 @@
     <h1 class="mx-auto max-w-md md:max-w-3xl relative z-10
       font-bold text-uh-indigo-600 text-center md:text-4xl xl:text-5xl text-3xl !leading-tight"
     >
-      Simple URL shortener <br>
-      <span class="font-thin text-black">for individuals &amp; businesses.</span>
+    {{__('homepage.simple_url')}} <br>
+      <span class="font-thin text-black">{{__('homepage.for_individuals')}} &amp; {{__('homepage.bussinesses')}}.</span>
     </h1>
   </div>
 
@@ -28,23 +28,23 @@
       <form method="post" action="{{route('createshortlink')}}" class="mb-4 mt-12" id="formUrl">
       @csrf
         <div class="mt-1 text-center">
-          <input type="text" name="long_url" id="inputSourceLink" value="{{ old('long_url') }}" placeholder="{{__('Shorten your link')}}"
+          <input type="text" name="long_url" id="inputSourceLink" value="{{ old('long_url') }}" placeholder="{{__('homepage.search_link')}}"
             class="w-full md:w-4/6 px-2 md:px-4 h-12 sm:h-14
               text-xl outline-none border border-gray-200 focus:border-uh-indigo-300 rounded-t-md md:rounded-l-md md:rounded-r-none">
           <button type="submit" id="actProcess"
             class="w-full md:w-1/6 h-12 sm:h-14 align-top rounded-t-none md:rounded-l-none md:rounded-r-md
               text-lg text-white bg-uh-indigo-600 hover:bg-uh-indigo-700 focus:bg-uh-indigo-600"
           >
-            {{__('Shorten')}}
+          {{__('homepage.shorten')}}
           </button>
         </div>
 
         <br>
 
         <div class="custom-url sm:mt-8">
-          <b>{{__('Custom URL (optional)')}}</b>
+          <b>{{__('homepage.custom_url')}}</b>
           <span class="block mb-4 font-light">
-            {{__('Replace clunky URLs with meaningful short links that get more clicks.')}}</span>
+            {{__('homepage.replace_url')}}</span>
           <div class="inline text-2xl">
             {{$_SERVER['SERVER_NAME']}}/</div>
           <input id="custom_key" name="custom_key"
